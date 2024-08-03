f46c76c4c30c7583fc4ae0f536faea5165f44cd5
Asanaliev Temirlan <tonyadam050607@gmail.com>
2024-08-03 18:05:08 +0600

    .

--- app/src/main/java/com/example/testgeekspro/data/model/CarsModel.kt
+++ app/src/main/java/com/example/testgeekspro/data/model/CarsModel.kt
@@ -1,7 +1,9 @@
 package com.example.testgeekspro.data.model
 
+import java.io.Serializable
+
 data class CarsModel(
     val make: String,
     val year: String,
     val image: String,
-)
+):Serializable
